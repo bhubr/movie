bd9738f91a52bfb44878e99954c627ef66ee13cf
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2018-04-12 15:45:33 +0200

    Add click event listeners on navbar links

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Navbar (commune à toutes les pages)
-const navbarHtml = /* @html */`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+const navbarHtml = /* @html */`<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
     <a class="navbar-brand" href="#">Movie App</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -84,12 +84,76 @@ const render = contentHtml => {
   mainDiv.innerHTML = navbarHtml + contentHtml + footerHtml
 }
 
-const showHome = () => {
-  render(homeHtml)
+/**
+ * Fonction qui calcule une URL relative
+ * à partir d'une URL absolu, par exemple /about
+ * à partir de http://localhost:4000/about
+ *
+ * Si le serveur est sur http://localhost:4000
+ * on dispose de cette valeur dans window.location.origin
+ * On peut dans l'URL absolue remplacer cette valeur par une
+ * chaîne vide pour obtenir le chemin relatif
+ */
+const getRelativePath = absoluteUrl => {
+  return absoluteUrl.replace(window.location.origin, '')
 }
 
+
+const setupLinkEvents = () => {
+    // Puis on met en place la gestion des évènements
+  // On a mis un id sur la nav pour la retrouver plus facilement
+  const navbar = document.getElementById('navbar')
+  // On prend tous les liens de la navbar
+  const navbarLinks = navbar.getElementsByTagName('a')
+  // On va attacher un gestionnaire (listener) d'évènements sur
+  // CHACUN des liens ainsi récupérés
+  for (link of navbarLinks) {
+    // 1er param: nom de l'évènement
+    // 2ème param: callback qui prend un objet Event en paramètre
+    link.addEventListener('click', e => {
+      // Il faut empêcher le comportement par défaut du lien, qui serait
+      // de sortir de la page actuelle et d'en demander une nouvelle
+      e.preventDefault()
+      // Les objets Event ont un attribut target: l'élément sur lequel a
+      // été déclenché l'évènement.
+      const link = e.target
+      // On récupère l'adresse du lien par son attribut href
+      // C'est quelque chose comme http://localhost:4000/about.html
+      const linkUrl = link.href
+      // On doit recevoir quelque chose comme /about.html
+      const relativeUrl = getRelativePath(linkUrl)
+
+      if(relativeUrl === '/index.html') {
+        showHome()
+      }
+      else if(relativeUrl === '/about.html') {
+        showAbout()
+      }
+    })
+  }
+}
+
+
+/**
+ * Fonction qui affiche la home page
+ */
+const showHome = () => {
+
+  // D'abord on affiche le HTML: remplacement de l'ancien contenu
+  render(homeHtml)
+
+  // Puis une fonction met en place la gestion des évènements
+  setupLinkEvents()
+
+}
+
+
+/**
+ * Fonction qui affiche la page About
+ */
 const showAbout = () => {
   render(aboutHtml)
+  setupLinkEvents()
 }
 
 showHome()
